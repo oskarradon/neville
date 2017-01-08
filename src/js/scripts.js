0945290cc7ca69673fe02ea947e1549807cf2aa8
Oskar <oskarradonkimball@gmail.com>
2017-01-07 20:32:38 -0800

    strings and numbers

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,18 +1,26 @@
 let images = document.querySelectorAll('div.ball');
-//
-// //
+
 Array.prototype.forEach.call(images, function(el){
 	el.onclick=function() {
+		this.style.display = 'none';
 		let number = this.id.toString().replace('ball','');
-		console.log(number);
+		console.log(number + 'type:' + typeof number);
+		console.log('length:' + images.length + 'type:' + typeof images.length);
+		if (number === images.length) {
+			console.log('ok cool');
+			number === 1;
+			this.id = 'ball' + number;
+			this.style.display = 'block';
+		} else {
+			number++;
+			this.id = 'ball' + number;
+			this.style.display = 'block';
+		}
 	}
 });
 
+// it could be prettier to loop over the characters in the string of the id and when one is a number change it to number++
 
-
-// // when ball is clicked
-// $('.ball').click(function() {
-//
-// })
-// // hide this ball
-// // show the next one
+// when ball is clicked
+// hide this ball
+// show the next one
